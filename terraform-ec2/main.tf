e28fda8e407a2bc6e2a7b00233c2af72e27a35f0
EC2 Default User <ec2-user@ip-172-31-66-171.ec2.internal>
2026-02-01 09:51:16 +0000

    Terraform Files Updated

--- terraform-ec2/main.tf
+++ terraform-ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "EC2-SRVR" {
   ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2 us-east-1
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.public_subnet.id
+  subnet_id              = data.aws_subnets.default.ids[0]  # First default subnet
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   key_name               = "vockey"
 
